fd35f24bae49e046834e757be76fb2bd1ca63347
AP3HIM <thefartz19@gmail.com>
2025-09-02 19:40:59 -0400

    reverting back to /api in the url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE = import.meta.env.VITE_BACKEND_BASE_URL || "https://api.sunsetuploader.com";
+const API_BASE = import.meta.env.VITE_BACKEND_BASE_URL || "https://api.sunsetuploader.com/api";
 
 export async function register({ username, email, password }) {
   const res = await fetch(`${API_BASE}/accounts/register/`, {
