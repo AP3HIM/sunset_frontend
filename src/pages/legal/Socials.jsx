e1bb0220032a74078f1cb0ff18b0c1b9a98f5d6a
AP3HIM <thefartz19@gmail.com>
2025-10-21 17:01:41 -0400

    links to socials and tiktok put instead of yt

--- src/pages/legal/Socials.jsx
+++ src/pages/legal/Socials.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaTwitter, FaInstagram, FaTikTok } from "react-icons/fa";
 import "../../css/pricing.css";
 
 export default function Socials() {
@@ -9,14 +9,14 @@ export default function Socials() {
         <h1>Socials</h1>
         <p>Follow us for updates, tips, and new feature releases:</p>
         <div className="social-links">
-          <a href="https://twitter.com/YOUR_HANDLE" target="_blank" rel="noopener noreferrer">
+          <a href="https://x.com/MarketXPulse" target="_blank" rel="noopener noreferrer">
             <FaTwitter size={24} />
           </a>
-          <a href="https://instagram.com/YOUR_HANDLE" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/sunsetuploader" target="_blank" rel="noopener noreferrer">
             <FaInstagram size={24} />
           </a>
-          <a href="https://youtube.com/YOUR_CHANNEL" target="_blank" rel="noopener noreferrer">
-            <FaYoutube size={24} />
+          <a href="https://www.tiktok.com/@sunset_uploader23" target="_blank" rel="noopener noreferrer">
+            <FaTikTok size={24} />
           </a>
         </div>
       </div>
